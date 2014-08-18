edd8142dff291c49260c1417cce728a6e8ef0ccf
diego.concha <diego.concha@alumnos.inf.utfsm.cl>
2014-08-17 21:03:49 -0400

    Agregado pantalla principal y botón para jugar, colocado las dimensiones que pidió el profesor en gameplay y en la pantalla y modificado los GUI Text para que no invadan la pantalla D:
    Sugiero hacer una copia de gameplay.unity por seguridad ._.

---
+++ Assets/MenuScript.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MenuScript : MonoBehaviour {
+
+	void OnGUI(){
+		if(GUI.Button(new Rect(Screen.width/2-40,Screen.height-60,80,20),"Play")){
+			Application.LoadLevel("gameplay");
+		}
+	}
+}
